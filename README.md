3f70827deead55cf170d67b972b7fbb7c5b7bece
Faizan Zafar <faizan.zafar@rwth-aachen.de>
2021-01-26 02:23:00 +0100

    update 26.01

---
+++ README.md
@@ -0,0 +1,43 @@
+# PMSIM: Automatic Discrete Event Simulation of Processes Using Event Logs
+This project provides an open-source tool that allows the user to generate an event log by simulating the discovered process model from an original event log recorded from a process aware information system. This tool is completely written in [Python](https://www.python.org/), using the [pm4py](http://pm4py.org/) library for process model generation and the [SimPy](https://pypi.org/project/simpy/) library for discrete event simulation. The tool mainly comprises three modules:
+* Process Mining
+  - In this module, the process model is discovered in the form of a Petri net by applying process mining techniques on the original event log. This Petri net presents the possible flow of activities for the cases. Furthermore, feature extraction is performed in the subsequent performance analyses step, resulting in the calculation of _arrival rate of cases_ and _activity duration_. These extracted features are subsequently used in the discrete event simulation module.
+* Discrete Event Simulation
+  - In this module, new cases are generated based on the features extracted from the process mining module. Users are also provided with the option to interact with the tool by providing as input, the number of cases to be generated. This input also acts as an endpoint for the simulation process. Additionally, users can modify the arrival rate of cases as well as the activity duration for particular activities.
+ * Generating the Simulated Event Logs
+   - In this module, the simulated events for the generated cases are transformed into an event log. This event log is saved in the form of a ```.csv``` file. Moreover, the discrete event simulation clock is converted into a real timestamp and records each activity for the cases accordingly.
+
+  ### Features 
+  * In the process discovery step, the presence of loops in the Petri net is handled with _maximum trace length_ which limits the execution of unrealistic loops for the simulated cases.
+  * The _arrival rate of cases_ is calculated by considering the _business hours_, which results in a more accurate value.
+  * The tool provides users the option to modify the features _case arrival time_ and _activity time_.
+  * The tool supports both ```.xes``` and ```.csv``` event log formats as input.
+  * The tool is capable of handling event logs with single as well as two timestamps i.e. when the start and complete timestamps are given in the event log, then the value of the average duration of each activity is calculated.
+  
+  ### Screencast
+  In [this](https://drive.google.com/file/d/1YVVF7_t8LNCEiwmNrgnNMJajL0nq19nI/view?usp=sharing) video, you can watch a screencast of the tool which demonstrates the main functionalities of our Python-based tool for automatic discrete event simulation of processes using event logs. Please note that you may need to download the video file to watch. 
+ 
+  ### Requirements
+  This open-source tool is OS-independent, and you only need to install the Python packages specified in the [requirements](https://github.com/mbafrani/AutomaticProcessSimulation/blob/master/requirements) file.
+  
+  ### Usage
+  As a preceding step, install the mentioned python packages using the following commands:
+  
+  ```shell
+  pip install pm4py
+  pip install simpy
+  ```
+  
+  Then, navigate to the directory where the original event log (_running-example.xes_ in our case) is stored and run the _simulation_activity.py_ file using the following command:
+  
+  ```shell
+  python simulation_activity.py running-example.xes
+  ```
+
+  Therewith, the simulation models of the discovered process model (including all the performance information) are produced and stored in the newly generated _methods.py_ file.
+  
+  Now, run the _simulation.py_ file which performs the actual simulation process (optionally with custom inputs provided by users) using the following command:
+  
+  ```shell
+  python simulation.py running-example.xes
+  ```
---
+++ simulation_activity.py
@@ -0,0 +1,268 @@
+import argparse
+import os
+import random
+from pathlib import Path
+from datetime import datetime, timedelta
+import statistics
+from pm4py.objects.log.importer.xes import factory as xes_import_factory
+from pm4py.objects.log.adapters.pandas import csv_import_adapter
+from pm4py.objects.conversion.log import factory as conversion_factory
+import math
+import warnings
+from pm4py.util import xes_constants
+from pm4py.util import constants
+import pandas as pd
+
+
+import scipy
+import scipy.stats
+import matplotlib
+import matplotlib.pyplot as plt
+
+case_id_key = xes_constants.DEFAULT_TRACEID_KEY
+activity_key = xes_constants.DEFAULT_NAME_KEY
+timestamp_key = datetime.now()
+results = []
+
+warnings.filterwarnings('ignore')
+
+
+def read_input_file_path():
+    """
+        Reads the input file path from the Command Line Interface and verifies if the file exists
+        Returns
+        --------------
+        file.file_path
+                The file path of the input event log file
+    """ 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file_path", type=Path)
+    file = parser.parse_args()
+    print("File received: ", file.file_path)
+    if file.file_path.exists():
+        print("File exists")
+    else:
+        print("File does not exist. Please input correct file")
+        exit()
+    return str(file.file_path)
+
+
+def import_xes(file_path):
+    """
+        Imports logs from the input xes file
+        Parameters
+        --------------
+        file_path
+            Path of the input event log file
+        Returns
+        --------------
+        xes_log
+            The input event logs in the form of a log
+        """
+    xes_log = xes_import_factory.apply(file_path)
+    print("Import of xes successful,with {0} traces in total".format(len(xes_log)))
+    return xes_log
+
+
+def import_csv(file_path):
+    """
+            Imports logs from the input csv file
+            Parameters
+            --------------
+            :param file_path:
+                The path to the csv log file
+            Returns
+            --------------
+            csv_log
+                The input event logs in the form of a log
+            """
+    data_frame = csv_import_adapter.import_dataframe_from_path(
+        os.path.join(file_path), sep=";")
+    data_frame["time:timestamp"] = data_frame["time:timestamp"].apply(lambda x:
+                                                                      datetime.strptime(x, '%d-%m-%Y:%H.%M'))
+    if 'time:complete' in data_frame.columns:
+        data_frame["time:complete"] = pd.to_datetime(data_frame["time:complete"], format='%d-%m-%Y:%H.%M')
+    data_frame["Activity"] = data_frame["concept:name"]
+    parameters = {constants.PARAMETER_CONSTANT_CASEID_KEY: "concept:name",
+                  constants.PARAMETER_CONSTANT_ACTIVITY_KEY: "activity",
+                  constants.PARAMETER_CONSTANT_TIMESTAMP_KEY: "time:timestamp"}
+    csv_log = conversion_factory.apply(data_frame, parameters=parameters)
+    print("Import of csv successful,with {0} traces in total".format(len(csv_log)))
+    return csv_log
+
+
+def verify_extension_and_import():
+    """
+            This function verifies that the extension of the event log file is .xes or .csv and imports the
+            logs from those files
+            Returns
+            --------------
+            log
+                The input event logs in the form of a log
+            """
+
+    file_path = read_input_file_path()
+    file_name, file_extension = os.path.splitext(file_path)
+    file_extension = file_extension.replace("'))", "")
+    print("File Extension: ", file_extension)
+    if file_extension == ".xes":
+        log = import_xes(file_path)
+        return log
+    elif file_extension == ".csv":
+        log = import_csv(file_path)
+        return log
+    else:
+        print("Unsupported extension. Supported file extensions are .xes and .csv ONLY")
+        exit()
+
+def remove_outliers(dataset, attribute):
+    
+    Q1 = dataset[attribute].quantile(0.25)
+    Q3 = dataset[attribute].quantile(0.75)
+    IQR = Q3 - Q1
+    
+    UpperWhisker = Q3 + 1.5 *IQR
+    LowerWhisker = Q1 - 1.5 *IQR
+    
+    filter = (dataset[attribute] > LowerWhisker) | (dataset[attribute] < UpperWhisker)
+    
+    result = dataset.loc[filter]   
+
+    return result
+    
+class Distribution(object):
+    def __init__(self, dist_name_list = []):
+        self.dist_names = ['norm','lognorm','expon']
+        self.dist_results = []
+        self.params = {}
+        
+        self.DistributionName = ""
+        self.PValue = 0
+        self.Param = None
+        
+        self.isFitted = False
+    
+    def Fit(self, y):
+        self.dist_results = []
+        self.params = {}
+        for dist_name in self.dist_names:
+            dist = getattr(scipy.stats, dist_name)
+            param = dist.fit(y)
+            self.params[dist_name] = param
+            #Applying the Kolmogorov-Smirnov test
+            D, p = scipy.stats.kstest(y, dist_name, args=param);
+            self.dist_results.append((dist_name,p))
+        #select the best fitted distribution
+        sel_dist,p = (max(self.dist_results,key=lambda item:item[1]))
+        #store the name of the best fit and its p value
+        self.DistributionName = sel_dist
+        self.PValue = p
+        
+        self.isFitted = True
+        #print("Best fitted distribution and the p value are:", self.DistributionName,self.PValue)
+       
+        return self.DistributionName,self.PValue
+    
+    
+def create_methods():
+    """
+                This function calculates the average time taken for each activity and writes methods to method.py file
+                to be used for simulation
+                """
+    log = verify_extension_and_import()
+    timetaken = {}
+    for trace in log:
+        length = len(trace)
+        for index, event in enumerate(trace):
+            if index < (length - 1):
+                next_event = trace[index + 1]
+                if "concept:name" in event:
+                    attribute = event["concept:name"]
+                    if "time:complete" in event:
+                        if attribute not in timetaken:
+                            timetaken[attribute] = [(event["time:complete"] - event["time:timestamp"]).total_seconds()]
+                        else:
+                            timetaken[attribute].append(
+                                (event["time:complete"] - event["time:timestamp"]).total_seconds())
+                    else:
+                        if "time:timestamp" in event:
+                            time = event["time:timestamp"]
+                        if "time:timestamp" in next_event:
+                            next_time = next_event["time:timestamp"]
+                        else:
+                            next_time = time
+                        if attribute not in timetaken:
+                            timetaken[attribute] = [(next_time - time).total_seconds()]
+                        else:
+                            timetaken[attribute].append((next_time - time).total_seconds())
+            else:
+                mean = statistics.mean(timetaken[attribute])
+                if "concept:name" in event:
+                    attribute = event["concept:name"]
+                if attribute not in timetaken:
+                    timetaken[attribute] = [mean]
+                else:
+                    timetaken[attribute].append(mean)
+    
+
+
+    dfn = pd.DataFrame.from_dict(timetaken, orient='index')
+    dfr = dfn.transpose()
+          
+    
+    for col in dfr.columns:         
+        cleaned = remove_outliers(dfr, col)
+
+    cleaned.dropna(inplace = True) 
+    new_timetaken = cleaned.to_dict('list')
+    #print(new_timetaken)
+    distribution = {}
+    for attribute in new_timetaken:
+        dst = Distribution()
+        distribution[attribute]=dst.Fit(new_timetaken[attribute])
+        new_timetaken[attribute] = random.choice(new_timetaken[attribute])
+    
+    user_req = "y"
+    user_input = input("Do you want to modify the average time for any activity? Enter y to modify or press any key to "
+                       "continue ")
+    if user_input.lower() == "y":
+        while user_req.lower() == "y":
+            print("Average time taken for each activity in seconds: ", new_timetaken)
+            print("Distribution of each activity and P-value:", distribution)
+            user_activity = input("Enter the activity you want to configure the average time taken ")
+            if user_activity not in new_timetaken:
+                print("No such activity found")
+            else:
+                print("Activity Found, Average time taken is ", new_timetaken[user_activity])
+                user_time = input("Enter the average time (in seconds) ")
+                new_timetaken[user_activity] = float(user_time)
+            user_req = input("Do you want to configure more activities? Enter y to configure or press any key to "
+                             "continue ")
+
+    attributes = {}
+    for trace in log:
+        for event in trace:
+            if "concept:name" in event:
+                attribute = event["concept:name"]
+                if attribute not in attributes:
+                    attributes[attribute] = math.ceil(new_timetaken[attribute])
+                # attributes[attribute] = attributes[attribute] + 1
+    print(attributes)
+
+    f = open("methods.py", "w")
+    f.write('''\
+class Trace(object):
+    def __init__(self,env):
+        self.env = env       
+    ''')
+    for attribute in attributes:
+        f.write('''\
+def %s(self):
+        \tyield self.env.timeout(%d)       
+    ''' % (str(attribute).replace(" ", ""), attributes[attribute]))
+    f.close()
+
+
+if __name__ == '__main__':
+    create_methods()
+
